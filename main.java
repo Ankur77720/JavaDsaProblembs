6763cbe9821d2314b2acb1031983ff9d69b9c34f
Ankur Prajapati <91510065+Ankur77720@users.noreply.github.com>
2023-01-12 01:35:40 +0530

    main

--- main.java
+++ main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class main {
 
-  public static void main(String[] args) {
+  public static void main2(String[] args) {
     Scanner sc = new Scanner(System.in);
     float Num1 = sc.nextFloat();
     float Num2 = sc.nextFloat();
