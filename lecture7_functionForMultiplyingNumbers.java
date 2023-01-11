6b11910496c381eaba2bc88d39f475e429723d72
Ankur Prajapati <91510065+Ankur77720@users.noreply.github.com>
2023-01-12 01:31:58 +0530

    lecture7

---
+++ lecture7_functionForMultiplyingNumbers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class lecture7_functionForMultiplyingNumbers {
+
+  public static int multiplyNumbers(int num1, int num2) {
+    int product;
+    product = num1 * num2;
+    return product;
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int num1 = sc.nextInt();
+    int num2 = sc.nextInt();
+    System.out.println(multiplyNumbers(num1, num2));
+    return;
+  }
+}
